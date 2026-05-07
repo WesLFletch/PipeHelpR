1dc3c5c6d085610a4d159123df28728285ac1f44
WesLFletch <weslfletch@tamu.edu>
2026-05-07 15:05:48 -0500

    WF do.R - Undo separate do() functions, new logic
    Instead of explicitly separating the functionality of do() into do() and do0(), internal logic checks whether f() is parameterless or one-parameter, and automatically behaves accordingly. This should be more cohesive with previous versions and remain interpretable for users.

--- R/do.R
+++ R/do.R
@@ -1,16 +1,12 @@
-#' A convenience function that applies a function to an input, a more readable shorthand for "checkpointing" with an anonymous function.
-#' @param x An object passed into `f`.
-#' @param f A function to apply on `x`.
-#' @return The output of `f(x)`, whatever that may be.
+#' A convenience function that either: a) applies a function to an input or b) runs a parameterless function. A useful shorthand alternative to piping into an anonymous function (a) or manually removing intermediate objects used in creating an output (b).
+#' @param x An object passed into `f` if `f` is not parameterless.
+#' @param f A one-argument or parameterless function. If one-argument, then `f(x)` is returned. Otherwise, `f()` is returned.
+#' @return The output of `f(x)` or `f()`, whatever that may be.
 #' @export
-do = function(x, f){
+do = function(x=NULL, f){
+  if (length(formals(f))==0){
+    if (!is.null(x)) warning("`x` was provided but `f` is parameterless, `x` will not be used")
+    return(f())
+  }
   return(f(x))
-}
-
-#' A convenience function performs a no-argument function, a useful shorthand for using a functional environment to prevent intermediate object leakage into the main environment.
-#' @param f A no-argument function to perform.
-#' @return The output of `f()`, whatever that may be.
-#' @export
-do0 = function(f){
-  return(f())
 }
